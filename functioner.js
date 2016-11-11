c2207b24801b357bb10816c9dbecc7a29d612685
CraigH2013 <craigharrison112@gmail.com>
2016-11-11 10:56:53 -0500

    added more functions

--- functioner.js
+++ functioner.js
@@ -1,4 +1,59 @@
+'use strict';
+
 var functioner = {
+  type: function(t) {
+    switch (t) {
+      case 'string':
+        return function(val) {
+          return typeof val === 'string';
+        };
+      case 'number':
+        return function(val) {
+          return typeof val === 'number';
+        };
+      case 'numeric':
+        return function(val) {
+          return !isNaN(parseFloat(val)) && isFinite(val);
+        };
+      case 'integer':
+      case 'int':
+        return function(val) {
+          return Number.isInteger(val);
+        };
+      case 'float':
+      case 'decimal':
+        return function(val) {
+          return typeof val === 'number' && !Number.isInteger(val);
+        };
+      case 'boolean':
+      case 'bool':
+        return function(val) {
+          return typeof val === 'boolean';
+        };
+      case 'object':
+      case 'obj':
+        return function(val) {
+          return typeof val === 'object';
+        };
+      case 'array':
+        return function(val) {
+          return Array.isArray(val);
+        };
+      case 'iterable':
+        return function(val) {
+          if (val === null || val === undefined) {
+            return false;
+          }
+          return typeof val[Symbol.iterator] === 'function';
+        };
+      case 'error':
+        return function(val) {
+          return val instanceof Error;
+        };
+      default:
+
+    }
+  },
   greaterThan: function(x) {
     return function(val) {
       return val > x;
@@ -31,16 +86,136 @@ var functioner = {
   lte: function(x) {
     return functioner.lessThanOrEqualTo(x);
   },
+  equalTo: function(x) {
+    return function(val) {
+      return val === x;
+    };
+  },
+  eq: function(x) {
+    return functioner.equalTo(x);
+  },
+  null: function(x) {
+    return x === null;
+  },
+  undefined: function(x) {
+    return x === undefined;
+  },
+  assigned: function(x) {
+    return x !== undefined;
+  },
+  add: function(x) {
+    return function(val) {
+      return val + x;
+    };
+  },
+  subtract: function(x) {
+    return function(val) {
+      return val - x;
+    };
+  },
+  divide: function(x) {
+    return function(val) {
+      return val / x;
+    };
+  },
+  multiply: function(x) {
+    return function(val) {
+      return val * x;
+    };
+  },
   accessor: function(access) {
     return function(obj) {
       return obj[access];
     };
+  },
+  has: function(prop) {
+    return function(obj) {
+      return obj.hasOwnProperty(prop);
+    };
+  },
+  index: function(i) {
+    return function(arr) {
+      return arr[i];
+    };
+  },
+  includes: function(x) {
+    return function(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === x) {
+          return true;
+        }
+      }
+      return false;
+    };
+  },
+  contains: function(x) {
+    return function(string) {
+      return string.includes(x);
+    };
+  },
+  match: function(regex) {
+    return function(string) {
+      return regex.test(string);
+    };
+  },
+  positive: function(x) {
+    return x > 0;
+  },
+  negative: function(x) {
+    return x < 0;
+  },
+  zero: function(x) {
+    return x === 0;
+  },
+  infinity: function(x) {
+    return x === Infinity;
+  },
+  finite: function(x) {
+    return x !== Infinity;
+  },
+  between: function(a, b, inclusive) {
+    var min = Math.min(a, b);
+    var max = Math.max(a, b);
+
+    if (inclusive === true) {
+      return function(val) {
+        return min <= val && val <= max;
+      };
+    } else {
+      return function(val) {
+        return min < val && val < max;
+      };
+    }
   },
   even: function(x) {
     return x % 2 === 0;
   },
   odd: function(x) {
     return x % 2 !== 0;
+  },
+  caesar: function(x) {
+    if (x < 0) {
+      x = x + 26;
+    }
+    return function(string) {
+      var output = '';
+
+      Array.from(string).forEach(char => {
+        if (char.match(/[a-z]/i)) {
+          var code = char.charCodeAt(0);
+
+          if (code >= 65 && code <= 90) {
+            char = String.fromCharCode(((code - 65 + x) % 26) + 65);
+          } else if (code >= 97 && code <= 122) {
+            char = String.fromCharCode(((code - 97 + x) % 26) + 97);
+          }
+        }
+
+        output +=  char;
+      });
+
+      return output;
+    };
   }
 };
 
--- usage.js
+++ usage.js
@@ -20,6 +20,31 @@ var numbers = [0, 1, 2, 3, 4, 5, 6];
 var lte3 = numbers.filter(f.lte(3));
 // console.log(lte3);
 
+// equalTo
+var numbers = [0, 1, 0, 1, 0, 1];
+var eq0 = numbers.filter(f.eq(0));
+// console.log(eq0);
+
+// add
+var numbers = [0, 1, 2, 3, 4, 5, 6];
+var add2 = numbers.map(f.add(2));
+// console.log(add2);
+
+// subtract
+var numbers = [0, 1, 2, 3, 4, 5, 6];
+var sub2 = numbers.map(f.subtract(2));
+// console.log(sub2);
+
+// divide
+var numbers = [1, 2, 3, 4];
+var div2 = numbers.map(f.divide(2));
+// console.log(div2);
+
+// multiply
+var numbers = [0, 1, 2, 3];
+var mult5 = numbers.map(f.multiply(5));
+// console.log(mult5);
+
 // accessor
 var people = [
   {name: 'Alex', sex: 'f'},
@@ -30,6 +55,66 @@ var people = [
 var names = people.map(f.accessor('name'));
 // console.log(names);
 
+// has
+var people = [
+  {name: 'Alex', sex: 'f'},
+  {name: 'Bob', sex: 'm', age: 46},
+  {name: 'Carla', sex: 'f'},
+  {name: 'Dan', sex: 'm', age: 25}
+];
+var hasAge = people.filter(f.has('age'));
+// console.log(hasAge);
+
+// index
+var matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+var firstCol = matrix.map(f.index(0));
+// console.log(firstCol);
+
+// includes
+var matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+var rowsWith4 = matrix.filter(f.includes(4));
+// console.log(rowsWith4);
+
+// contains
+var strings = ['hey', 'how', 'are', 'you', 'doing'];
+var os = strings.filter(f.contains('o'));
+// console.log(os);
+
+// match
+var strings = ['hey', 'how', 'are', 'you', 'doing'];
+var os = strings.filter(f.match(/.o/));
+// console.log(os);
+
+// positive
+var numbers = [0, -1, 2, -3, 4, -5, 6];
+var positives = numbers.filter(f.positive);
+// console.log(positives);
+
+// negative
+var numbers = [0, -1, 2, -3, 4, -5, 6];
+var negatives = numbers.filter(f.negative);
+// console.log(negatives);
+
+// zero
+var numbers = [0, -1, 0, -0, 4, -5, 6];
+var zeros = numbers.filter(f.zero);
+// console.log(zeros);
+
+// infinity
+var numbers = [0, 1, 2, 1/0];
+var infinites = numbers.filter(f.infinity);
+// console.log(infinites);
+
+// finite
+var numbers = [0, 1, 2, 1/0];
+var finites = numbers.filter(f.finite);
+// console.log(finites);
+
+// between
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var between3_7 = numbers.filter(f.between(3, 7));
+// console.log(between3_7);
+
 // even
 var numbers = [0, 1, 2, 3, 4, 5, 6];
 var evens = numbers.filter(f.even);
@@ -39,3 +124,8 @@ var evens = numbers.filter(f.even);
 var numbers = [0, 1, 2, 3, 4, 5, 6];
 var odds = numbers.filter(f.odd);
 // console.log(odds);
+
+// caeser
+var strings = ['my', 'secret', 'messages'];
+var secret = strings.map(f.caesar(13));
+// console.log(secret);
